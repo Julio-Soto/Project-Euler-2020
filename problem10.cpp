f405b912c2dd43282dea4c8c31a44976aa4fab60
Julio Soto <juliosotodev@gmail.com>
2020-01-22 06:17:27 -0700

    Improved solution #10

--- problem10.cpp
+++ problem10.cpp
@@ -1,4 +1,14 @@
+/*
+
+        The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+        Find the sum of all the primes below two million.
+
+*/
+
 #include<iostream>
+#include<math.h>
+#include<vector>
 
 using namespace std;
 
@@ -6,6 +16,8 @@ bool isPrime(int testNumber){
     int i = 2;
     bool isPrime = true;
 
+    // Brute force iteration to find out
+    //if the test number is evenly divisible by any number.
     while(isPrime == true && i <= testNumber/2){
         if(testNumber % i == 0){ isPrime = false;}
         ++i;
@@ -13,20 +25,38 @@ bool isPrime(int testNumber){
 
     return isPrime;
 }
+/*
+    SOLUTION:
+    Since building a sequence of primes takes too many computations.
+    Build a Sieve of composites that in consequence is also a sieve of PRIMES.
 
+*/
 int main(){
     cout << "Hello Project Euler!" << endl;
     cout << "Problem # 10" << endl;
     cout << "Summation of Primes" << endl;
 
     long long int summation = 0;
+    int n = 2000000;
+    vector<int> sieve(n,NULL);
+    double sqrtOfn = sqrt(n);
+    sieve[1] = 1; //initial value for the sieve
 
-    for(int k = 2 ;k < 2000000; ++k){
-        if(isPrime(k)) summation += k;
+    for(int k = 4;k < sieve.size(); k+=2)  //fill the sieve with all the even composites from 4 to n
+        sieve[k] = k;
+
+    for(int p = 3 ;p <= sqrtOfn ; ++p){   //Check for primes only to sqrt(n)
+        if(isPrime(p)){
+                for(int i = p;i <= n/p; i+=2)  // use those primes to generate all the composites from 3 to n
+                sieve[p * i] = (p * i);   //fill the sieve with all the odd composites
+        }
     }
 
-    cout << "The sum of all primes bellow 2,000,000 is: " << summation << endl;
+    for(int k = 2;k < sieve.size(); ++k) // the sieve has all the composites so
+       if(sieve[k] == NULL) summation += k; // all NULL values in the sieve correspond to a PRIME NUMBER index.
+
+    cout << "The sum of all primes bellow " << n << " is: " << summation << endl;
 
     return 0;
 }
-//142913828922
+// soln: 142913828922
